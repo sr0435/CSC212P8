6eb836bb2185047872cd5f7c8639edaec04bfc31
savitharavi <savitharavi@131.229.110.217>
2019-04-08 16:35:58 -0400

    need to do count nodes, binary search, and LLHash

--- src/main/java/edu/smith/cs/csc212/p8/CheckSpelling.java
+++ src/main/java/edu/smith/cs/csc212/p8/CheckSpelling.java
@@ -30,6 +30,40 @@ public class CheckSpelling {
 		return words;
 	}
 	
+	public static List<String> loadMisspelled() {
+		long start = System.nanoTime();
+		List<String> misspelled;
+		try {
+			// Read from a file:
+			misspelled = Files.readAllLines(new File("src/main/resources/misspelled").toPath());
+		} catch (IOException e) {
+			throw new RuntimeException("Couldn't find dictionary.", e);
+		}
+		long end = System.nanoTime();
+		double time = (end - start) / 1e9;
+		System.out.println("Loaded " + misspelled.size() + " entries in " + time +" seconds.");
+		return misspelled;
+	}
+	
+	public static List<String> loadBook(String filePath) {
+		long start = System.nanoTime();
+		List<String> book = new ArrayList<>();
+		try {
+			// Read from a file:
+			for(String line : Files.readAllLines(new File(filePath).toPath())) {
+				book.addAll(WordSplitter.splitTextToWords(line));
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Couldn't find dictionary.", e);
+		}
+		long end = System.nanoTime();
+		double time = (end - start) / 1e9;
+		System.out.println("Loaded " + book.size() + " entries in " + time +" seconds.");
+		return book;
+	}
+	
+	
+	
 	/**
 	 * This method looks for all the words in a dictionary.
 	 * @param words - the "queries"
@@ -55,14 +89,26 @@ public class CheckSpelling {
 	public static List<String> createMixedDataset(List<String> yesWords, int numSamples, double fractionYes) {
 		// Hint to the ArrayList that it will need to grow to numSamples size:
 		List<String> output = new ArrayList<>(numSamples);
+		double numYesWords = numSamples * fractionYes;
+		double numNoWords = numSamples - numYesWords;
+		for (int i=0;i<numSamples;i++) {
+			if (i<numYesWords) {
+				output.add(yesWords.get(i));
+			}
+			else {
+				String wrong = yesWords.get(i);
+				wrong = wrong.concat("lrglk");
+				output.add(wrong);
+			}
+		}
 		// TODO: select numSamples * fractionYes words from yesWords; create the rest as no words.
 		return output;
 	}
 	
-	
 	public static void main(String[] args) {
 		// --- Load the dictionary.
 		List<String> listOfWords = loadDictionary();
+		List<String> nonsense = loadMisspelled();
 		
 		// --- Create a bunch of data structures for testing:
 		TreeSet<String> treeOfWords = new TreeSet<>(listOfWords);
@@ -77,25 +123,91 @@ public class CheckSpelling {
 			hm100k.add(w);
 		}
 		
-		// --- Make sure that every word in the dictionary is in the dictionary:
-		timeLookup(listOfWords, treeOfWords);
-		timeLookup(listOfWords, hashOfWords);
-		timeLookup(listOfWords, bsl);
-		timeLookup(listOfWords, trie);
-		timeLookup(listOfWords, hm100k);
+		// --- Testing the insert speed
+			// HashSet and TreeSet constructed with input data
+		/*
+		 * long start = System.nanoTime(); TreeSet<String> treeOfWords2 = new
+		 * TreeSet<>(listOfWords); long end = System.nanoTime(); double time = (end -
+		 * start) / 1e9; double timePer = time / 235886;
+		 * System.out.println("TreeSet Time Per Element:" + timePer);
+		 * System.out.println("TreeSet With Input Data Fill Time:" + time);
+		 * 
+		 * start = System.nanoTime(); HashSet<String> hashOfWords2 = new
+		 * HashSet<>(listOfWords); end = System.nanoTime(); time = (end - start) / 1e9;
+		 * timePer = time / 235886; System.out.println("HashSet Time Per Element:" +
+		 * timePer); System.out.println("HashSet With Input Data Fill Time:" + time);
+		 * 
+		 * start = System.nanoTime(); SortedStringListSet bsl2 = new
+		 * SortedStringListSet(listOfWords); end = System.nanoTime(); time = (end -
+		 * start) / 1e9; timePer = time / 235886;
+		 * System.out.println("SSLSet Time Per Element:" + timePer);
+		 * System.out.println("SSLSet With Input Data Fill Time:" + time);
+		 * System.out.println("\n");
+		 */
 		
+			// Now everything is made with a loop
+		/*
+		 * start = System.nanoTime(); TreeSet<String> treeOfWords3 = new TreeSet<>();
+		 * for (String w : listOfWords) { treeOfWords3.add(w); } end =
+		 * System.nanoTime(); time = (end - start) / 1e9; timePer = time / 235886;
+		 * System.out.println("TreeSet (Loop) Time Per Element:" + timePer);
+		 * System.out.println("TreeSet With Loop Time:" + time);
+		 * 
+		 * start = System.nanoTime(); HashSet<String> hashOfWords3 = new HashSet<>();
+		 * for (String w : listOfWords) { hashOfWords3.add(w); } end =
+		 * System.nanoTime(); time = (end - start) / 1e9; timePer = time / 235886;
+		 * System.out.println("HashSet (Loop) Time Per Element:" + timePer);
+		 * System.out.println("HashSet With Loop Time:" + time);
+		 * 
+		 * start = System.nanoTime(); CharTrie trie2 = new CharTrie(); for (String w :
+		 * listOfWords) { trie2.insert(w); } end = System.nanoTime(); time = (end -
+		 * start) / 1e9; timePer = time / 235886;
+		 * System.out.println("CharTrie Time Per Element:" + timePer);
+		 * System.out.println("CharTrie With Loop Time:" + time);
+		 * 
+		 * start = System.nanoTime(); LLHash hm100k2 = new LLHash(100000); for (String w
+		 * : listOfWords) { hm100k2.add(w); } end = System.nanoTime(); time = (end -
+		 * start) / 1e9; timePer = time / 235886;
+		 * System.out.println("LLHash Time Per Element:" + timePer);
+		 * System.out.println("LLHash With Loop Time:" + time);
+		 */
+		
+		// --- Make sure that every word in the dictionary is in the dictionary:
+		//timeLookup(listOfWords, treeOfWords);
+		//timeLookup(listOfWords, hashOfWords);
+		//timeLookup(listOfWords, bsl);
+		//timeLookup(listOfWords, trie);
+		//timeLookup(listOfWords, hm100k);
+		System.out.println("\n");
+		
+		// checks how many words in misspelled aren't in the dictionary
+		/*
+		 * System.out.println("Starting Nonsense:"); timeLookup(nonsense, treeOfWords);
+		 * timeLookup(nonsense, hashOfWords); timeLookup(nonsense, bsl);
+		 * timeLookup(nonsense, trie); timeLookup(nonsense, hm100k);
+		 * System.out.println("\n");
+		 */
+		
+		/*
+		 * System.out.println("Earnest"); List<String> earnest =
+		 * loadBook("src/main/resources/theImportanceOf"); timeLookup(earnest,
+		 * treeOfWords); timeLookup(earnest, hashOfWords); timeLookup(earnest, bsl);
+		 * timeLookup(earnest, trie); timeLookup(earnest, hm100k);
+		 * System.out.println("\n");
+		 */
 		
 		for (int i=0; i<10; i++) {
 			// --- Create a dataset of mixed hits and misses with p=i/10.0
 			List<String> hitsAndMisses = createMixedDataset(listOfWords, 10_000, i/10.0);
 			
 			// --- Time the data structures.
-			timeLookup(hitsAndMisses, treeOfWords);
-			timeLookup(hitsAndMisses, hashOfWords);
-			timeLookup(hitsAndMisses, bsl);
-			timeLookup(hitsAndMisses, trie);
-			timeLookup(hitsAndMisses, hm100k);
+			/*
+			 * timeLookup(hitsAndMisses, treeOfWords); timeLookup(hitsAndMisses,
+			 * hashOfWords); timeLookup(hitsAndMisses, bsl); timeLookup(hitsAndMisses,
+			 * trie); timeLookup(hitsAndMisses, hm100k); System.out.println("\n");
+			 */
 		}
+		System.out.println("\n");
 			
 
 		
